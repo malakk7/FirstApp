5da42ddd5c5cd180f21b5b4b56e02c86b0ce483e
malakk73 <MAL20190001@std.psut.edu.jo>
2022-11-01 18:19:41 +0200

    currency conversion app

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,36 +1,94 @@
 package com.example.firstapp
 
+import android.widget.EditText
+import android.widget.TextView
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.*
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity<string> : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val button: Button = findViewById(R.id.btResult) // to variable button to btResult
-        button.setOnClickListener{ view ->
-            var x: Int = edtxt1.text.toString().toInt();
-            var y: Int = edtxt2.text.toString().toInt()
-            resultTV.text = sum(x,y).toString();
 
+        val edtxt1: EditText = findViewById(R.id.Numtochange)
+        val button: Button = findViewById(R.id.btResult) // to variable button to btResult
+        val textResult: TextView = findViewById(R.id.textResult)
+
+        var flag:String ="USD"
+        val spinnerVal :Spinner =findViewById(R.id.from)
+        var options =arrayOf("USD","EUR","CAD","SAR")
+        spinnerVal.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options )
+
+        //spinnerVal.adapter= ArrayAdapter<string>(this,android.R.layout.simple_list_item_1,options)
+
+        spinnerVal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+               flag=options.get(position)//position is the index in the array
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+
+        }
+        button.setOnClickListener{ view ->
+            var x: Float = edtxt1.text.toString().toFloat();
+            if(flag=="USD")
+                textResult.text=USDtoJOD(x).toString();
+            else if(flag=="EUR")
+                textResult.text=EURtoJOD(x).toString();
+            else if (flag=="CAD")
+                textResult.text=CADtoJOD(x).toString();
+            else if(flag=="SAR")
+                textResult.text=SARtoJOD(x).toString();
         }
 
     }
-}
-public fun USDtoJOD(a:Float):Float{
+
+  //??? create function for arrayadapter???  private fun <T> ArrayAdapter(mainActivity: MainActivity<T>, simpleListItem1: Int, options: Array<String>): SpinnerAdapter? {
+
+    }
+
+    public fun USDtoJOD(a:Float):Float{
     val b=0.709f;
     return a*b;
-}
-public fun JODtoUSD(a:Float):Float{
-    val c=0.709f;
-    return a/c;
 }
 public fun EURtoJOD(a:Float):Float{
     val b=0.70502f;
     return a*b;
 }
+public fun CADtoJOD(a:Float):Float{
+    val b=0.520f;
+    return a*b;
+}
+public fun SARtoJOD(a:Float):Float{
+    val b=0.189f;
+    return a*b;
+}
+
+
+public fun JODtoUSD(a:Float):Float{
+    val c=0.709f;
+    return a/c;
+}
+
 public fun JODtoEUR(a:Float):Float{
     val c=0.70502f;
     return a/c;
